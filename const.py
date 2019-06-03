28221f4d11dcc1470cedff3e8ea50c063bec8cf7
gengen <is0408hp@ed.ritsumei.ac.jp>
2019-06-04 02:40:52 +0900

    [add] add comments

--- const.py
+++ const.py
@@ -1,10 +1,10 @@
 #!usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-SR = 16000
-H = 512
-FFT_SIZE = 1024
+SAMPLING_RATE = 16000        # サンプリング周波数:16kHz
+HOP_LENGTH = 512             # 窓関数を掛ける時の移動(ずらし)幅
+FFT_SIZE = 1024              # 高速フーリエ変換(FFT)を行うときの幅
 BATCH_SIZE = 64
 PATCH_LENGTH = 128
 
-PATH_FFT = './spectrogram'
+PATH_FFT = './spectrogram/'  # spectrogramを保存するディレクトリ
