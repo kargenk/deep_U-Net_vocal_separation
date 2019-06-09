79672b792bbafe69b9baf64372d5f77f35f2fa0b
gengen <is0408hp@ed.ritsumei.ac.jp>
2019-06-09 23:03:08 +0900

    [add] audio path for save

--- const.py
+++ const.py
@@ -1,10 +1,11 @@
 #!usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-SAMPLING_RATE = 16000        # サンプリング周波数:16kHz
-HOP_LENGTH = 512             # 窓関数を掛ける時の移動(ずらし)幅
-FFT_SIZE = 1024              # 高速フーリエ変換(FFT)を行うときの幅
+SAMPLING_RATE = 16000              # サンプリング周波数:16kHz
+HOP_LENGTH = 512                   # 窓関数を掛ける時の移動(ずらし)幅
+FFT_SIZE = 1024                    # 高速フーリエ変換(FFT)を行うときの幅
 BATCH_SIZE = 64
 PATCH_LENGTH = 128
 
-PATH_FFT = './spectrogram/'  # spectrogramを保存するディレクトリ
+PATH_FFT = './spectrogram/'        # spectrogramを保存するディレクトリ
+PATH_AUDIO = './src/audio_check/'  # データセットを保存するディレクトリ
