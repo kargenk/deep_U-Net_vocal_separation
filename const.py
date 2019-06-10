8b43d7c20635a90478473aa6f817cf92e1b0b759
gengen <is0408hp@ed.ritsumei.ac.jp>
2019-06-10 19:35:57 +0900

    [add] image height and width size

--- const.py
+++ const.py
@@ -4,8 +4,10 @@
 SAMPLING_RATE = 16000              # サンプリング周波数:16kHz
 HOP_LENGTH = 512                   # 窓関数を掛ける時の移動(ずらし)幅
 FFT_SIZE = 1024                    # 高速フーリエ変換(FFT)を行うときの幅
+IMAGE_HEIGHT = 512                 # 入力画像の縦サイズ
+IMAGE_WIDTH = 128                  # 入力画像の横サイズ
 BATCH_SIZE = 64
 PATCH_LENGTH = 128
 
-PATH_FFT = './spectrogram/'        # spectrogramを保存するディレクトリ
+PATH_FFT = 'E:/Datasets/sound/MedleyDB/spectrograms'        # spectrogramを保存するディレクトリ
 PATH_AUDIO = './src/audio_check/'  # データセットを保存するディレクトリ
