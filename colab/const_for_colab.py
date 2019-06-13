ec46653840a1542d2bcfcaeca5087ca47ae2bf84
gengen <is0408hp@ed.ritsumei.ac.jp>
2019-06-14 00:51:02 +0900

    [fix] hop length

--- colab/const_for_colab.py
+++ colab/const_for_colab.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 SAMPLING_RATE = 16000              # サンプリング周波数:16kHz
-HOP_LENGTH = 512                   # 窓関数を掛ける時の移動(ずらし)幅
+HOP_LENGTH = 768                   # 窓関数を掛ける時の移動(ずらし)幅
 FFT_SIZE = 1024                    # 高速フーリエ変換(FFT)を行うときの幅
 IMAGE_HEIGHT = 512                 # 入力画像の縦サイズ
 IMAGE_WIDTH = 128                  # 入力画像の横サイズ
